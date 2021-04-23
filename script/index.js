525ffa2a8c1b1c80b180c1a61c6197baf2cc2457
Aleksandra <samsonova183@gmail.com>
2021-04-23 16:31:52 +0300

    second review

--- script/index.js
+++ script/index.js
@@ -1,22 +1,21 @@
 let edit = document.querySelector('.profile__edit');
 let popup = document.querySelector('.popup');
 let closePopunButton = document.querySelector('.popup__close');
-let newName = document.querySelector('.popup__name');
-let newDescription = document.querySelector('.popup__description');
+let newValue = document.querySelectorAll('.popup__text');
 let profileName = document.querySelector('.profile__title');
 let profileDescription = document.querySelector('.profile__subtitle');
 function openPopup(){
     popup.classList.add('popup_opened');
-    newName.value = profileName.textContent;
-    newDescription.value = profileDescription.textContent;
+    newValue.item(0).value = profileName.textContent;
+    newValue.item(1).value = profileDescription.textContent;
 }
 function closedPopup(){
     popup.classList.remove('popup_opened');
 }
 function saveData(evt){
     evt.preventDefault();
-    profileName.textContent = newName.value;
-    profileDescription.textContent = newDescription.value;
+    profileName.textContent = newValue.item(0).value;
+    profileDescription.textContent = newValue.item(1).value;
     closedPopup();
 }
 edit.addEventListener('click',openPopup);
