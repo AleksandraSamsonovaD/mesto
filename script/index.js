0524320124709c4697e63c079e58b15a06b14d75
Aleksandra <samsonova183@gmail.com>
2021-04-24 11:47:45 +0300

    third review

--- script/index.js
+++ script/index.js
@@ -1,23 +1,25 @@
 let edit = document.querySelector('.profile__edit');
 let popup = document.querySelector('.popup');
 let closePopunButton = document.querySelector('.popup__close');
-let newValue = document.querySelectorAll('.popup__text');
 let profileName = document.querySelector('.profile__title');
 let profileDescription = document.querySelector('.profile__subtitle');
+let form = document.querySelector('.popup__container');
+let newName = document.querySelector('[name = "profile-name"]');
+let newDescription = document.querySelector('[name = "profile-description"]');
 function openPopup(){
     popup.classList.add('popup_opened');
-    newValue.item(0).value = profileName.textContent;
-    newValue.item(1).value = profileDescription.textContent;
+    newName.value = profileName.textContent;
+    newDescription.value = profileDescription.textContent;
 }
 function closedPopup(){
     popup.classList.remove('popup_opened');
 }
 function saveData(evt){
     evt.preventDefault();
-    profileName.textContent = newValue.item(0).value;
-    profileDescription.textContent = newValue.item(1).value;
+    profileName.textContent = newName.value;
+    profileDescription.textContent  = newDescription.value;
     closedPopup();
 }
 edit.addEventListener('click',openPopup);
-popup.addEventListener('submit',saveData);
+form.addEventListener('submit',saveData);
 closePopunButton.addEventListener('click',closedPopup);
